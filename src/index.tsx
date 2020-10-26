e85b595c8c2b7ce2222a3ce16eb2c37b8f18fe00
Diogo Silva <diogosilva@MacBook-Pro-de-Diogo.local>
2020-10-26 18:28:23 +0000

    Export components to be used by the library

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,20 @@
 import Button from './Button/Button.component';
+import Checkbox from './Checkbox/Checkbox.component';
+import Input from './Input/Input.component';
+import Label from './Label/Label.component';
+import Modal from './Modal/Modal.component';
+import Select from './Select/Select.component';
+import Tabs from './Tabs/Tabs.component';
+import Toogle from './Toogle/Toogle.component';
 
-export { Button };
+
+export {
+    Button,
+    Checkbox,
+    Input,
+    Label,
+    Modal,
+    Select,
+    Tabs,
+    Toogle
+};
